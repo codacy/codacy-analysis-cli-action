8f83882935e44a26b3c273828902d5b72d10aff2
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2020-09-18 11:58:39 +0100

    feature: Add github action definition and docs

--- README.md
+++ README.md
@@ -1,8 +1,67 @@
-# codacy-public-template
+# GitHub Action for Codacy Analysis CLI
 
-Template repository for new public repositories. 
+This is a GitHub Action for invoking the [Codacy Analysis CLI](https://github.com/codacy/codacy-analysis-cli) and returning identified issues in the code.
 
-## GitHub actions
+## GitHub Action
 
-This repository has the common GitHub actions that we want to have accross all of our public repositories.
-They should be kept at `.github/workflows`
+The following is an example of a workflow using the CLI as an action,
+to analyse each commit and pull request.
+
+```yaml
+name: codacy-analysis-cli
+
+on: ["push"]
+
+jobs:
+  codacy-analysis-cli:
+    runs-on: ubuntu-latest
+    name: Codacy Analysis CLI
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@master
+      - name: Run codacy-analysis-cli
+        uses: codacy/codacy-analysis-cli-action@master
+```
+
+Running the action with the default configurations will:
+
+- Analyse the current commit or pull request running all supported tools, with the default configuration,
+  for the languages you are using.
+- Print analysis results into the console (you can check them in GitHub's action workflow panel).
+- Fail the workflow if at least one issue is found in your code.
+
+Check the next section to see how you can further configure this action.
+
+### Caveats
+
+This action supports all [CLI configuration options](https://github.com/codacy/codacy-analysis-cli#commands-and-configuration) with the following exceptions:
+
+- `--commit-uuid` -- **Not supported**. The action will only analyse the commit that triggered it.
+- `--api-token` -- **Not supported**. Use [`--project-token`](https://github.com/codacy/codacy-analysis-cli#project-token) instead.
+- `--username` -- **Not supported**. Use [`--project-token`](https://github.com/codacy/codacy-analysis-cli#project-token) instead.
+- `--project` -- **Not supported**. Use [`--project-token`](https://github.com/codacy/codacy-analysis-cli#project-token) instead.
+
+The command `validate-configuration` is also **not supported**.
+
+When using `--project-token` make sure to use [GitHub security features](https://docs.github.com/en/actions/reference/encrypted-secrets)
+to prevent you from committing a secret token to your code. For example, if you store your Codacy project
+token in GitHub, this is how you would use it in the action workflow.
+
+```yaml
+# ...
+uses: codacy/codacy-analysis-cli-action@master
+with:
+    project-token: ${{ secrets.<PROJECT_TOKEN_NAME> }}
+# ...
+```
+
+## Contributing
+
+We love contributions, feedback, and bug reports.
+For issues with the invocation of this action,
+file [issues](https://github.com/codacy/codacy-analysis-cli-action/issues) in this repository.
+
+
+## More Information
+
+For documentation on Codacy itself, including policy language and capabilities see the [Codacy Documentation](https://docs.codacy.com)
---
+++ codacy-analysis-cli.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+
+#
+# Codacy Analysis CLI Wrapper
+#
+
+log_error() {
+  local message=$1
+
+  cat >&2 <<-EOF
+		We encountered a problem with your Docker setup:
+		  > ${message}
+		
+		Please check https://github.com/codacy/codacy-analysis-cli for alternative instructions.
+		
+	EOF
+  exit 3
+}
+
+test_docker_socket() {
+  if [ ! -S /var/run/docker.sock ]; then
+    log_error "/var/run/docker.sock must exist as a Unix domain socket"
+  elif [ -n "${DOCKER_HOST}" ] && [ "${DOCKER_HOST}" != "unix:///var/run/docker.sock" ]; then
+    log_error "invalid DOCKER_HOST=${DOCKER_HOST}, must be unset or unix:///var/run/docker.sock"
+  fi
+}
+
+run() {
+  local output_volume="";
+  if [ -n "${OUTPUT_DIRECTORY}" ]; then
+    output_volume="--volume ${OUTPUT_DIRECTORY}:${OUTPUT_DIRECTORY}";
+  fi
+  local CODACY_ANALYSIS_CLI_VERSION="${CODACY_ANALYSIS_CLI_VERSION:-stable}"
+  docker run \
+    --rm \
+    --env CODACY_CODE="$CODACY_CODE" \
+    --env CODACY_PROJECT_TOKEN="$CODACY_PROJECT_TOKEN" \
+    --env CODACY_API_TOKEN="$CODACY_API_TOKEN" \
+    --env CODACY_API_BASE_URL="$CODACY_API_BASE_URL" \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    --volume "$CODACY_CODE":"$CODACY_CODE" \
+    ${output_volume} \
+    --volume /tmp:/tmp \
+    codacy/codacy-analysis-cli:${CODACY_ANALYSIS_CLI_VERSION} -- \
+    "$@"
+}
+
+analysis_file() {
+  local filename="";
+  local is_filename=0;
+  for arg; do
+    case "$arg" in
+      -*)
+        case ${arg} in
+          -d | --directory)
+            is_filename=1 # next argument will be the directory or file
+            ;;
+        esac
+        ;;
+      *)
+        if [ ${is_filename} -eq 1 ]; then
+          if [ -n "$filename" ]; then
+            echo "Please provide only one file or directory to analyse" >&2
+            exit 1
+          else
+            is_filename=0
+            filename="$arg"
+          fi
+        fi
+        ;;
+    esac
+  done
+
+  if [ -n "$filename" ]; then
+    CODACY_CODE="$filename"
+  else
+    CODACY_CODE="$(pwd)"
+  fi
+}
+
+prep_args_with_output_absolute_path() {
+  local is_filename=0;
+  local new_args="";
+  for arg; do
+    case "$arg" in
+      -*)
+        case ${arg} in
+          --output)
+            is_filename=1 # next argument will be the file
+            ;;
+        esac
+        new_args="${new_args} ${arg}"
+        ;;
+      *)
+        if [ ${is_filename} -eq 1 ]; then
+          if [ -n "$OUTPUT" ]; then
+            echo "Please provide only one output file" >&2
+            exit 1
+          else
+            is_filename=0
+            OUTPUT_DIRECTORY="$(cd $(dirname "${arg}") && pwd -P)"
+            OUTPUT_FILENAME="$(basename "${arg}")"
+            OUTPUT="${OUTPUT_DIRECTORY}/${OUTPUT_FILENAME}"
+            new_args="${new_args} ${OUTPUT}"
+          fi
+        else
+          new_args="${new_args} ${arg}"
+        fi
+        ;;
+    esac
+  done
+
+  ARGUMENTS_WITH_ABSOLUTE_PATH_OUTPUT="$new_args"
+}
+
+test_docker_socket
+
+analysis_file "$@"
+
+prep_args_with_output_absolute_path "$@"
+
+run $ARGUMENTS_WITH_ABSOLUTE_PATH_OUTPUT
